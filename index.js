5a6afb3c640cf2e3c3641afaabae8ec7c4d4a8d0
janmejaybhavsar <89321629+janmejaybhavsar@users.noreply.github.com>
2022-02-22 11:42:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world');// You can use apostrophes, quotation marks or tildees which are `s
+var http = require("http");
+
+http.createServer(function (request, response) {
+
+           response.writeHead(200, {'Content-Type': 'text/plain'});
+
+           response.end("Content is visible");          
+
+}).listen(8080);
+
+console.log('Server Running');
